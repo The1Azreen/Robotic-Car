0f80fa7cb8c93b4cd48d13fad06483f840fee896
The1Azreen <2200581@sit.singaporetech.edu.sg>
2024-11-11 16:51:40 +0800

    Update pins.h

--- pins.h
+++ pins.h
@@ -1,14 +1,18 @@
 #ifndef PINSALOC_H_
 #define PINSALOC_H_
 
+//Encoder Pins Left
 #define WHEEL_ENCODER_LEFT_PIN 5
+
+// Wheel Motor Pins Left
 #define WHEEL_LEFT_PWN_PIN 4
-#define WHEEL_LEFT_OUT_PIN_1 2
+#define WHEEL_LEFT_OUT_PIN_1 2 //Might be reversef
 #define WHEEL_LEFT_OUT_PIN_2 3
 
-//Encoder Pins
+//Encoder Pins Right
 #define WHEEL_ENCODER_RIGHT_PIN 7
 
+// Wheel Motor Pins Right
 #define WHEEL_RIGHT_PWN_PIN 6
 #define WHEEL_RIGHT_OUT_PIN_1 8
 #define WHEEL_RIGHT_OUT_PIN_2 9
