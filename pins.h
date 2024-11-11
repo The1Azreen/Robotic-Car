9b4d630198263649cf487fed8118ceb5d835f5d6
The1Azreen <2200581@sit.singaporetech.edu.sg>
2024-11-11 16:49:09 +0800

    Update pins.h

--- pins.h
+++ pins.h
@@ -6,12 +6,17 @@
 #define WHEEL_LEFT_OUT_PIN_1 2
 #define WHEEL_LEFT_OUT_PIN_2 3
 
+//Encoder Pins
 #define WHEEL_ENCODER_RIGHT_PIN 7
+
 #define WHEEL_RIGHT_PWN_PIN 6
 #define WHEEL_RIGHT_OUT_PIN_1 8
 #define WHEEL_RIGHT_OUT_PIN_2 9
 
+//Buzzer Pin Remove if not used
 #define BUZZER_PIN 18
+
+//Ultrasonic Pins
 #define TRIG_PIN 0
 #define ECHO_PIN 1
 
